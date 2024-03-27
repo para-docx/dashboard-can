e76d8f4ee4af6239b6599237a09b7990043e8fd0
para-docx <froezendecember2001@gmail.com>
2024-03-27 23:27:05 +0530

    Co-authored-by: Barbatoss <Barbatoss@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
+import ReactSpeedometer from 'react-d3-speedometer';
+import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  const [data, setSensdata] = useState(null);
+
+  useEffect(() => {
+    axios.get('https://api.thingspeak.com/channels/1835714/feeds.json?api_key=UG41IELRYIJHDTJR&results=2')
+      .then(response => {
+        setSensdata(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+  
+  // console.log(sendata.feeds[0]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data && (
+        <div>
+          <h2>Gauge for {data.channel.field1}</h2>
+          <ReactSpeedometer
+            maxValue={100} // Adjust max value based on your data
+            value={parseInt(data.feeds[0].field1)} // Assuming field1 holds the gauge value
+            needleColor="green"
+            startColor="blue"
+            segments={10}
+            endColor="red"
+            needleTransitionDuration={4000}
+            needleTransition="easeElastic"
+            textColor="black"
+          />
+          <h2>Gauge for {data.channel.field2}</h2>
+          <ReactSpeedometer
+            maxValue={100} // Adjust max value based on your data
+            value={parseInt(data.feeds[0].field2)} // Assuming field2 holds the gauge value
+            needleColor="blue"
+            startColor="green"
+            segments={10}
+            endColor="red"
+            needleTransitionDuration={4000}
+            needleTransition="easeElastic"
+            textColor="black"
+          />
+          {/* Add more gauges for additional fields if needed */}
+        </div>
+      )}
     </div>
   );
 }
